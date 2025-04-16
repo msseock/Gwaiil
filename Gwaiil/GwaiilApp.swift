656b93ccc01463f2851ba5324957d9906d858143
SSOL <msseock@gmail.com>
2025-04-16 09:55:57 +0900

    Created Project

---
+++ Gwaiil/GwaiilApp.swift
@@ -0,0 +1,17 @@
+//
+//  GwaiilApp.swift
+//  Gwaiil
+//
+//  Created by 석민솔 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GwaiilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
